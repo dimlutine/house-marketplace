f283ea3401c226c51c209b1a772da613e46d376c
Jeremy Rowley <dimlutine@gmail.com>
2022-05-31 19:16:30 -0400

    post-init cleanup

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBK3RthJ6qShdd9hOCHdB38FWms56hcnPs",
+  authDomain: "house-marketplace-app-45ab1.firebaseapp.com",
+  projectId: "house-marketplace-app-45ab1",
+  storageBucket: "house-marketplace-app-45ab1.appspot.com",
+  messagingSenderId: "978664536636",
+  appId: "1:978664536636:web:d6e0fc6789945e5ae52139"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore()
