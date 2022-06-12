2bdcb100a0299643f4f154f312bbea51a0e87cd3
Jeremy Rowley <dimlutine@gmail.com>
2022-06-12 15:34:45 -0400

    address issue quick fix

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -110,7 +110,7 @@ function CreateListing() {
     } else {
       geolocation.lat = latitude;
       geolocation.lng = longitude;
-      location = address;
+
     }
 
     //Store image in firebase
@@ -174,9 +174,10 @@ function CreateListing() {
       timestamp: serverTimestamp()
     }
 
+    formDataCopy.location = address
     delete formDataCopy.images
     delete formDataCopy.address
-    location && (formDataCopy.location = location)
+
     !formDataCopy.offer && delete formDataCopy.discountedPrice
 
     const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
